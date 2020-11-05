8fae192a7ab93c121585a5c0d9364c6ee97245c8
Bastien REYS <breys@sollyazar.com>
2020-11-05 17:13:36 +0100

    ajout jeu de données

--- breys-equestre/src/main/resources/data.sql
+++ breys-equestre/src/main/resources/data.sql
@@ -23,3 +23,21 @@ insert into USER values ('100010', 'monitor1@gmail.com', 'moniteur', false, 'Mhu
 insert into USER values ('100011', 'monitor2@gmail.com', 'moniteur', false, 'Dheu', '', 0, '$2a$10$9mUFcMRX/JrFCtN24WU/seoFerZCEWx./EVpG8AdQdkMySCwH8Oh2', '0808080809', 'USER_MONITOR', null);
 insert into USER values ('100012', 'monitor3@gmail.com', 'moniteur', false, 'Troua', '', 0, '$2a$10$9mUFcMRX/JrFCtN24WU/seoFerZCEWx./EVpG8AdQdkMySCwH8Oh2', '0808080800', 'USER_MONITOR', null);
 insert into USER values ('100013', 'monitor4@gmail.com', 'moniteur', false, 'Katre', '', 0, '$2a$10$9mUFcMRX/JrFCtN24WU/seoFerZCEWx./EVpG8AdQdkMySCwH8Oh2', '0808080801', 'USER_MONITOR', null);
+
+-- TEST
+insert into REPRISE values('4999', '2020-11-06', 'monitor1@gmail.com', '2020-11-25', 'Galop 6', 0, 5, 'Reprise 0');
+insert into REPRISE values('4998', '2020-11-06', 'monitor2@gmail.com', '2020-11-25', 'Galop 6', 0, 5, 'Reprise 0');
+-- FIN TEST
+insert into REPRISE values('5000', '2020-11-17', 'monitor1@gmail.com', '2020-11-25', 'Galop 1', 0, 3, 'Reprise 1');
+insert into REPRISE values('5001', '2020-11-15', 'monitor1@gmail.com', '2020-11-25', 'Galop 3', 0, 7, 'Reprise 2');
+insert into REPRISE values('5002', '2020-11-15', 'monitor2@gmail.com', '2020-11-25', 'Galop 7', 0, 7, 'Reprise 1');
+insert into REPRISE values('5003', '2020-11-16', 'monitor3@gmail.com', '2020-11-25', 'Galop 2', 0, 4, 'Reprise 1');
+insert into REPRISE values('5004', '2020-11-16', 'monitor4@gmail.com', '2020-11-25', 'Galop 6', 0, 5, 'Reprise 1');
+
+-- id, age, email_cavalier, taille, nom, nbLoses, nbWins, sexe, poids 
+insert into HORSE values('6000', '8', null, 143, 'Nuage', 0, 0, 'MALE', 95);
+insert into HORSE values('6001', '7', null, 130, 'Neige', 1, 0, 'FEMELLE', 140);
+insert into HORSE values('6002', '6', null, 128, 'Soleil', 2, 2, 'FEMELLE', 120);
+
+insert into AFFILIATION values('7000', 'reys3.bastien@gmail.com', '4999');
+insert into AFFILIATION values('7001', 'reys2.bastien@gmail.com', '4999');
